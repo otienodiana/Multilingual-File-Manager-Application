731bc8f002067f5da1f570e23ea721b9e2344c25
Your Name <you@example.com>
2024-07-21 02:50:39 +0300

    update the routes

--- file_manager/utils/db.js
+++ file_manager/utils/db.js
@@ -1,22 +1,55 @@
-import { Sequelize } from 'sequelize';
+import { MongoClient } from 'mongodb';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  logging: false,
-});
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
-const dbClient = {
-  isAlive: () => sequelize.authenticate().then(() => true).catch(() => false),
-  nbUsers: async () => {
-    // Replace with actual query
-    const [results] = await sequelize.query('SELECT COUNT(*) AS count FROM users');
-    return results[0].count;
-  },
-  nbFiles: async () => {
-    // Replace with actual query
-    const [results] = await sequelize.query('SELECT COUNT(*) AS count FROM files');
-    return results[0].count;
-  },
-};
+/**
+ * Class for performing operations with Mongo service
+ */
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
+
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
+  isAlive() {
+    return !!this.db;
+  }
+
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
+  async nbUsers() {
+    const numberOfUsers = await this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
+
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
+  async nbFiles() {
+    const numberOfFiles = await this.filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+}
+
+const dbClient = new DBClient();
 
 export default dbClient;
--- file_manager/utils/redis.js
+++ file_manager/utils/redis.js
@@ -1,14 +1,34 @@
-import Redis from 'ioredis';
+const redis = require('redis');
+const { promisify } = require('util');
 
-const redisClient = new Redis();
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient().on('error', (error) => console.log(error));
+  }
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
 
-const redisClientWrapper = {
-  isAlive: () => redisClient.status === 'ready',
-  // Add other redis related functions here
-};
+  async get(key) {
+    // convert the get method to return a promise
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-export default redisClientWrapper;
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    return setAsync(key, value, 'Ex', duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
